79eed46fc7b51cf73419b57fb8754576741b2fff
GinoFRavera <ginofrg@live.com.ar>
2022-06-24 20:48:14 -0300

    React Parte 1

---
+++ Modulo 4 Unidad 1/TransportesX/frontend/src/pages/NosotrosPage.js
@@ -0,0 +1,26 @@
+const NosotrosPage = (props) => {
+    return (
+        <main className="holder">
+            <div className="historia">
+                <h2>Historia</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean dui nisl, volutpat eu fringilla non, feugiat a quam. Nunc lacinia erat ante, id sodales purus aliquam condimentum. Nulla odio magna, dignissim at nunc in, egestas dictum est. Phasellus ac sem nisl. Aliquam ut dapibus leo. Vivamus in porta sapien. In tempor est at interdum tristique.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean dui nisl, volutpat eu fringilla non, feugiat a quam. Nunc lacinia erat ante, id sodales purus aliquam condimentum. Nulla odio magna, dignissim at nunc in, egestas dictum est. Phasellus ac sem nisl. Aliquam ut dapibus leo. Vivamus in porta sapien. In tempor est at interdum tristique.</p>
+            </div>
+            <div className="staff">
+                <h2>Staff</h2>
+                <div className="personas">
+                    <div className="persona">
+                        <img src="img/nosotros/nosotros1.jpg" alt="Juan Gomez"/>
+                    
+                    <h5>Juan Gomez</h5>
+                    <h6>Gerente General</h6>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean dui nisl, volutpat eu fringilla non, feugiat a quam. Nunc lacinia erat ante, id sodales purus aliquam condimentum. Nulla odio magna, dignissim at nunc in, egestas dictum est. Phasellus ac sem nisl. Aliquam ut dapibus leo. Vivamus in porta sapien. In tempor est at interdum tristique.</p>
+
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
+}
+
+export default NosotrosPage;
